218149ceb33cf69ceb1ba5715e37043b70884990
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-03-17 19:36:12 +0200

    Add project files.

---
+++ пр hashtable/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace пр_hashtable
+{
+    public partial class Form1 : Form
+    {
+        Hashtable students = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text;
+            int grade = int.Parse(textBox2.Text);
+            string roomNumber = textBox3.Text;
+
+            students.Add(roomNumber, new Student { Surname = surname, Grade = grade });
+
+            listBox1.Items.Clear();
+            foreach (DictionaryEntry entry in students)
+            {
+                listBox1.Items.Add($"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string roomNumber = textBox3.Text;
+            if (students.ContainsKey(roomNumber))
+            {
+                students.Remove(roomNumber);
+            }
+
+            listBox1.Items.Clear();
+            foreach (DictionaryEntry entry in students)
+            {
+                listBox1.Items.Add($"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}");
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text;
+            string message = "";
+            foreach (DictionaryEntry entry in students)
+            {
+                if (((Student)entry.Value).Surname == surname)
+                {
+                    message += $"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}\n";
+                }
+            }
+            MessageBox.Show(message);
+        }
+    }
+}
+
---
+++ пр hashtable/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_hashtable
+{
+    public class Student
+    {
+        public string Surname { get; set; }
+        public int Grade { get; set; }
+    }
+}
