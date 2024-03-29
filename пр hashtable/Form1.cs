0131ce767206c7beabb9b34ac0d5278511be07b6
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-03-29 23:11:55 +0200

    ЛАСТ ТЕЙБЛ

--- пр hashtable/Form1.cs
+++ пр hashtable/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,37 +20,57 @@ namespace пр_hashtable
         {
             InitializeComponent();
         }
+        // Метод для додавання або редагування студента
+        private void AddOrUpdateStudent(string roomNumber, string surname, int grade)
+        {
+            if (students.ContainsKey(roomNumber))
+            {
+                students[roomNumber] = new Student { Surname = surname, Grade = grade };
+            }
+            else
+            {
+                students.Add(roomNumber, new Student { Surname = surname, Grade = grade });
+            }
+            UpdateListBox();
+        }
 
+        // Метод для видалення студента
+        private void RemoveStudent(string roomNumber)
+        {
+            if (students.ContainsKey(roomNumber))
+            {
+                students.Remove(roomNumber);
+                UpdateListBox();
+            }
+        }
+
+        // Метод для оновлення списку у ListBox
+        private void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (DictionaryEntry entry in students)
+            {
+                listBox1.Items.Add($"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}");
+            }
+        }
+        // Додавання студента
         private void button1_Click_1(object sender, EventArgs e)
         {
             string surname = textBox1.Text;
             int grade = int.Parse(textBox2.Text);
             string roomNumber = textBox3.Text;
 
-            students.Add(roomNumber, new Student { Surname = surname, Grade = grade });
-
-            listBox1.Items.Clear();
-            foreach (DictionaryEntry entry in students)
-            {
-                listBox1.Items.Add($"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}");
-            }
+            AddOrUpdateStudent(roomNumber, surname, grade);
         }
 
+        // Видалення студента
         private void button2_Click(object sender, EventArgs e)
         {
             string roomNumber = textBox3.Text;
-            if (students.ContainsKey(roomNumber))
-            {
-                students.Remove(roomNumber);
-            }
-
-            listBox1.Items.Clear();
-            foreach (DictionaryEntry entry in students)
-            {
-                listBox1.Items.Add($"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}");
-            }
+            RemoveStudent(roomNumber);
         }
 
+        // Пошук студента
         private void button3_Click_1(object sender, EventArgs e)
         {
             string surname = textBox1.Text;
@@ -64,24 +85,14 @@ namespace пр_hashtable
             MessageBox.Show(message);
         }
 
+        // Редагування студента
         private void button4_Click(object sender, EventArgs e)
         {
             string surname = textBox1.Text;
             int grade = int.Parse(textBox2.Text);
             string roomNumber = textBox3.Text;
 
-            if (students.ContainsKey(roomNumber))
-            {
-                students[roomNumber] = new Student { Surname = surname, Grade = grade };
-            }
-
-            listBox1.Items.Clear();
-            foreach (DictionaryEntry entry in students)
-            {
-                listBox1.Items.Add($"{entry.Key}: {((Student)entry.Value).Surname}, {((Student)entry.Value).Grade}");
-            }
-
+            AddOrUpdateStudent(roomNumber, surname, grade);
         }
     }
 }
-
